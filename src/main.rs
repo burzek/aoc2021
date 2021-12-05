117832df1e0d94f458cce28167490b74d801e33f
boris.brinza <boris.brinza@morosystems.cz>
2021-12-05 23:35:20 +0100

    day 5

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::fs;
-use crate::day4::{day4_task1_task2};
+//use crate::day4::{day4_task1_task2};
+use crate::day5::{day5_task1, day5_task2};
 
-// mod day1;
-// mod day2;
+//mod day1;
+//mod day2;
 //mod day3;
-mod day4;
+mod day5;
 
 fn main() {
     // day1_task1(read_input("src/inputs/day1.input"));
@@ -13,7 +14,8 @@ fn main() {
     // day2_task2(read_input("src/inputs/day2.input"));
     //day3_task1(read_input("src/inputs/day3.input"));
     //day3_task2(read_input("src/inputs/day3.input"));
-    day4_task1_task2(read_input("src/inputs/day4.input"));
+    day5_task1(read_input("src/inputs/day5.input"));
+    day5_task2(read_input("src/inputs/day5.input"));
 }
 
 pub fn read_input(file_name : &str) -> String {
