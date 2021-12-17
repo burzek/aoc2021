0436bf000f785edaf2b84d66d7f0f22e50028f09
boris.brinza <boris.brinza@morosystems.cz>
2021-12-17 09:21:19 +0100

    day 10

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use std::fs;
-use crate::day10::{day10_task1, day10_task2};
-//use crate::day4::{day4_task1_task2};
-//use crate::day5::{day5_task1, day5_task2};
-// use crate::day6::{day6_task1, day6_task2};
-// use crate::day7::{day7_task1, day7_task2};
-// use crate::day9::{day9_task1, day9_task2};
+use crate::day13::day13_task1;
 
 
 //mod day1;
 //mod day2;
 //mod day3;
 //mod day5;
-mod day6;
-mod day7;
-mod day9;
-mod day10;
+// mod day6;
+// mod day7;
+// mod day9;
+// mod day10;
+mod day13;
 
 fn main() {
     // day1_task1(read_input("src/inputs/day1.input"));
@@ -27,8 +23,9 @@ fn main() {
     // day6_task2(read_input("src/inputs/day6.input"));
     // day9_task1(read_input("src/inputs/day9.input"));
     // day9_task2(read_input("src/inputs/day9.input"));
-    day10_task1(read_input("src/inputs/day10.input"));
-    day10_task2(read_input("src/inputs/day10.input"));
+    //day10_task1(read_input("src/inputs/day10.input"));
+    //day10_task2(read_input("src/inputs/day10.input"));
+    day13_task1(read_input("src/inputs/day13.input"));
 }
 
 pub fn read_input(file_name : &str) -> String {
